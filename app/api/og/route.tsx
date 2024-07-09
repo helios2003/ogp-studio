ba1bd615bb6e39c005e829268ea5713b5dcacae5
Ankit Dash <ankitdash2019@gmail.com>
2024-07-09 14:44:29 +0530

    add some more updates to the parser

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,38 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { ImageResponse } from 'next/og';
-import parseURL from '@/app/helpers/parser';
-import { DocumentInfo } from '@/app/types';
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function GET(req: NextRequest): Promise<ImageResponse | NextResponse> {
   try {
     const searchParams = req.nextUrl.searchParams;
-    const encodedDocument = searchParams.get('base64');
-    let ans = '';
-    if (encodedDocument) {
-      const info: DocumentInfo = await parseURL(encodedDocument);
-      ans = JSON.stringify(info);
-    }
-    
-    // const title = searchParams.has('title')
-    //   ? (searchParams.get('title') as string)
-    //   : null;
-    // const description = searchParams.has('description')
-    //   ? (searchParams.get('description') as string)
-    //   : null;
-    // const numServers = searchParams.has('numServers')
-    //   ? (searchParams.get('numServers') as string)
-    //   : null;
-    // const numChannels = searchParams.has('numChannels')
-    //   ? (searchParams.get('numChannels') as string)
-    //   : null;
 
-    // JSON.stringify({
-    //   info
-    // });
-    const { image } = { 
-      image: 'https://svgshare.com/i/145Z.svg' 
+    const title = searchParams.has('title')
+      ? (searchParams.get('title') as string)
+      : null;
+    const description = searchParams.has('description')
+      ? (searchParams.get('description') as string)
+      : null;
+    const numServers = searchParams.has('numServers')
+      ? (searchParams.get('numServers') as string)
+      : null;
+    const numChannels = searchParams.has('numChannels')
+      ? (searchParams.get('numChannels') as string)
+      : null;
+
+    const { image } = {
+      image: 'https://svgshare.com/i/145Z.svg'
     };
 
     return new ImageResponse(
@@ -47,23 +36,23 @@ export async function GET(req: NextRequest): Promise<ImageResponse | NextRespons
             justifyContent: 'center',
             backgroundColor: '#fff',
             backgroundImage: `url(${image})`,
+            textAlign: 'center',
             fontSize: 32,
             fontWeight: 600,
           }}
         >
-          {/* <div>
-            {title ? `Title: ${title}` : null}
+          <div style={{fontSize: '48'}}>
+            {title ? `Title: ${title.replace(/["']/g, '')}` : null}
           </div>
           <div>
-            {description ? `Description: ${description}` : null}
+            {description ? `Description: ${description.replace(/["']/g, '')}` : null}
           </div>
           <div>
             {numServers ? `Number of Servers: ${numServers}` : null}
           </div>
           <div>
             {numChannels ? `Number of Channels: ${numChannels}` : null}
-          </div> */}
-          {ans}
+          </div>
         </div>
       )
     );
--- app/types.tsx
+++
@@ -1,7 +0,0 @@
-export interface DocumentInfo {
-    title? : string,
-    version? : string,
-    description? : string,
-    numServers? : number,
-    numChannels? : number
-}
