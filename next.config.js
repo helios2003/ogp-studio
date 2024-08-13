7de259fc51684e24e29fb77975e1d19a8dc06b72
Ankit Dash <ankitdash2019@gmail.com>
2024-08-13 22:10:24 +0530

    fix caching

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
     return [
       {
-        source: '/api/og',
+        source: '/:path*',
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public,  max-age=3600, s-maxage=3600, stale-while-revalidate=3600, vary=user-agent'
+            value:
+              'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600',
+          },
+          {
+            key: 'Vary',
+            value: 'User-Agent, Path',
           },
         ],
       },
     ];
   },
 };
- 
+
 module.exports = nextConfig;
